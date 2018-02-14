316e4733bdbe208dcfb52fcba5a7639a1424fb7e
Armando <Armando.balkaran@gmail.com>
2018-02-14 15:18:01 +0100

    start

--- Discord Bot/Program.cs
+++ Discord Bot/Program.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
 
 namespace Discord_Bot
 {
-    class Program
-    {
-        static void Main(string[] args)
+    class Program { 
+
+
+        private readonly string _token = "NDEzMzM1MjI5NTUwNTU5MjM1.DWXUCw.UNwIIdRpImlTDPB9swwhpxkF_J0";
+
+        private DiscordSocketClient _client;
+
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            string token = "abcdefg..."; // Remember to keep this private!
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
